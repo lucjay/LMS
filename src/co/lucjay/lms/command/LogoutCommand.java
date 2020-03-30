8cbbce1570b0d7340c5e9a9dd3dc126ca6a3d0f3
User <User@YD01-04>
2020-03-30 17:39:46 +0900

    1

--- src/co/lucjay/lms/command/LogoutCommand.java
+++ src/co/lucjay/lms/command/LogoutCommand.java
@@ -12,8 +12,9 @@ public class LogoutCommand implements Command {
 	@Override
 	public String exec(HttpServletRequest request, HttpServletResponse response) throws ServerException, IOException {
 
-		HttpSession session = request.getSession(false);
-		session.invalidate();
+		// 세션타임은 web.xml에서도 설정가능하고 디폴트는30초
+		HttpSession session = request.getSession(false); // HttpSession session; < 이렇게 해도되긴함 그렇지만 안정적으로는 리퀘스트 겟세션사용
+		session.invalidate(); // 서버에서 세션을 삭제
 		return "home.do";
 	}
 
